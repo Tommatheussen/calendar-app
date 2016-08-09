2b016be2bf3d855cd152b8be29bf2451879a03e1
Tom Matheussen <tommatheussen5@hotmail.com>
2016-08-09 20:31:35 +0200

    Added calendar component and selections

---
+++ app/calendar/calendar.component.js
@@ -0,0 +1,57 @@
+'use strict';
+
+angular.
+    module('calendar').
+    component('calendar', {
+        templateUrl: 'calendar/calendar.template.html',
+        controller: calendarController,
+        controllerAs: 'vm'
+    });
+
+function calendarController() {
+    var vm = this;
+
+    vm.days = getDaysInMonth(new Date().getMonth() - 1, new Date().getFullYear());
+
+    vm.shifts = [
+        'v\'',
+        'kv',
+        'h1',
+        'Gv',
+        'klv',
+        'l\'',
+        'gl',
+        'hmlw',
+        'vw',
+        'ld',
+        'dw',
+    ];
+
+    vm.setShift = function (day, shift) {
+        console.log(day, shift);
+        day.shift === shift ? day.shift = null : day.shift = shift;
+    }
+
+    function getDaysInMonth(month, year) {
+        let date = new Date(year, month, 1);
+        let days = [];
+    
+        let offsetDays = (date.getDay() || 7) - 1;
+
+        // Add offset days to the days array, this ensures the 1st day of the month is placed at the correct position in the calendar view    
+        for (var i = 0; i < offsetDays; i++) {
+            days.push({
+                offset: true
+            });
+        }
+
+        while (date.getMonth() === month) {
+            days.push({
+                date: new Date(date.toString())
+            });
+            date.setDate(date.getDate() + 1);
+        }
+        console.log(days);
+        return days;
+    }
+}
