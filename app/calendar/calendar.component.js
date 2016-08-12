aae0fc4984a43455ae7c157dee29097f25d98b93
Tom Matheussen <tommatheussen5@hotmail.com>
2016-08-12 17:29:59 +0200

    Changed how offset days are loaded and represented

--- app/calendar/calendar.component.js
+++ app/calendar/calendar.component.js
@@ -18,34 +18,54 @@ function calendarController($mdMedia, $scope, GApi, calendarService) {
 	vm.saveShifts = saveShifts;
 
     function onInit() {
-        GApi.executeAuth('calendar', 'calendarList.list').then(function (resp) {
+        var today = new Date();
+        vm.offsetDays = getOffsetDays(today.getMonth() - 1, today.getUTCFullYear());
+        vm.days = getDaysInMonth(today.getMonth() -1, today.getUTCFullYear());
+
+        /*GApi.executeAuth('calendar', 'calendarList.list').then(function (resp) {
             vm.calendars = resp.items;
         }, function (error) {
             console.log(error);
 			});
-
+*/
 		calendarService.getShifts().then(function (shifts) {
 			vm.shifts = shifts;
 		});
     }
 
-	function saveShifts() {
-		GApi.executeAuth('calendar', 'events.insert', {
+    vm.getRange = function (num) {
+        console.log(num);
+        return new Array(num);
+    }
+
+    function saveShifts() {
+        console.log(vm.days);
+
+        
+        
+        
+		/*GApi.executeAuth('calendar', 'events.insert', [{
 			calendarId: "hj3i0ucmkenfjmdbrr85v7o2q8@group.calendar.google.com",
 			start: {
 				"dateTime": new Date()
 			},
 			end: {
-				"dateTime": new Date(new Date().setHours("15"))
-			}
-		}).then(function (resp) {
+				"dateTime": new Date(new Date().setHours("17"))
+            }
+        }, {
+            calendarId: "hj3i0ucmkenfjmdbrr85v7o2q8@group.calendar.google.com",
+			start: {
+				"dateTime": new Date(new Date().setHours("21"))
+			},
+			end: {
+				"dateTime": new Date(new Date().setHours("22"))
+            }    
+        }]).then(function (resp) {
 			console.log(resp);
 		}, function (error) {
 			console.log(error);
-		});
+		});*/
 	}
-
-    vm.days = getDaysInMonth(new Date().getMonth() - 1, new Date().getFullYear());
 
 	vm.smallScreen = $mdMedia('xs');
 
@@ -56,22 +76,12 @@ function calendarController($mdMedia, $scope, GApi, calendarService) {
 	});
 
     vm.setShift = function (day, shift) {
-        console.log(day, shift);
         day.shift === shift ? day.shift = null : day.shift = shift;
     }
 
     function getDaysInMonth(month, year) {
         var date = new Date(year, month, 1);
         var days = [];
-
-        var offsetDays = (date.getDay() || 7) - 1;
-
-        // Add offset days to the days array, this ensures the 1st day of the month is placed at the correct position in the calendar view
-        for (var i = 0; i < offsetDays; i++) {
-            days.push({
-                offset: true
-            });
-        }
 
         while (date.getMonth() === month) {
             days.push({
@@ -80,5 +90,12 @@ function calendarController($mdMedia, $scope, GApi, calendarService) {
             date.setDate(date.getDate() + 1);
         }
         return days;
+    }
+
+    function getOffsetDays(month, year) {
+        var date = new Date(year, month, 1);
+
+        var offsetDays = (date.getDay() || 7) - 1;
+        return offsetDays;
     }
 }
