b504049df0a5668a0a78a20c48d18a2ccb787a8d
Tom Matheussen <tommatheussen5@hotmail.com>
2016-08-12 17:08:51 +0200

    Updated the JSON shifts file

---
+++ app/calendar/calendar.filter.js
@@ -0,0 +1,15 @@
+'use strict';
+
+angular.module('calendar')
+    .filter('duration', function () {
+        return function (input) {
+            var duration = input.start.hour;
+            duration += ":";
+            duration += input.start.minute;
+            duration += "-";
+            duration += input.end.hour;
+            duration += ":";
+            duration += input.end.minute;
+            return duration;
+        }
+	});
