994a4696118464a4aa1826ed3a97d0866fcd8d9d
Tom Matheussen <tmatheussen@avia-gis.com>
2016-08-17 14:07:18 +0200

    Correctly show Error objects in the dialog as well as the http failure requests

--- app/error-handler/error-handler.config.js
+++ app/error-handler/error-handler.config.js
@@ -7,10 +7,27 @@
 
 	function errorConfig($provide) {
 		$provide.decorator("$exceptionHandler", function ($delegate, $injector) {
+			if (!('toJSON' in Error.prototype)) {
+				Object.defineProperty(Error.prototype, 'toJSON', {
+					value: function () {
+						var alt = {};
+
+						Object.getOwnPropertyNames(this).forEach(function (key) {
+							alt[key] = this[key];
+						}, this);
+
+						return alt;
+					},
+					configurable: true,
+					writable: true
+				});
+			}
+
 			return function (exception, cause) {
 				var errorService = $injector.get("errorService");
 				console.log("got exception: ", exception, cause);
-				errorService.addToast(exception);
+				var json = JSON.parse(JSON.stringify(exception));
+				errorService.addToast(json);
 				$delegate(exception, cause);
 			};
 		});
